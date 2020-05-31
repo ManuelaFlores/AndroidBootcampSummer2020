f83ed54e4f0b673813468a307bc3637408aa7fbd
Manuela Flores <fmanuela499@gmail.com>
2020-05-30 20:26:23 -0500

    update(main): try first flow :construction:

--- week1/Blackjack/src/Main.kt
+++ week1/Blackjack/src/Main.kt
@@ -2,4 +2,7 @@ import utils.*
 
 fun main() {
     val globalDeck = createDeck()
+
+    val hand = dealHand(globalDeck.toMutableList())
+    printResults(hand = hand, totalSumOfHand = evaluateHand(hand))
 }
