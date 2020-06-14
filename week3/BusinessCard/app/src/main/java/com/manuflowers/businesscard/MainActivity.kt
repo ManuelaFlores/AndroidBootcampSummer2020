0501e55da72d3d8e62bb77552bf955d8dde38eb2
Manuela Flores <fmanuela499@gmail.com>
2020-06-14 17:42:31 -0500

    feat(app): add more info alert dialog :art: :sparkles:
     - add new alert dialog.
     - add new font resources.

--- week3/BusinessCard/app/src/main/java/com/manuflowers/businesscard/MainActivity.kt
+++ week3/BusinessCard/app/src/main/java/com/manuflowers/businesscard/MainActivity.kt
@@ -2,6 +2,8 @@ package com.manuflowers.businesscard
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,5 +17,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.bottom_menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        if (item.itemId == R.id.info) {
+            showInfo()
+        }
+        return true
+    }
+
+    private fun showInfo() {
+        val dialogTitle = getString(R.string.about_title)
+        val dialogMessage = getString(R.string.about_message, BuildConfig.VERSION_NAME)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
     }
 }
