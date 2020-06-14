0059fc6fb4f4d92654b5b53de08e8b5530669c5c
Manuela Flores <fmanuela499@gmail.com>
2020-06-14 16:10:47 -0500

    add menu options

--- week3/BusinessCard/app/src/main/java/com/manuflowers/businesscard/MainActivity.kt
+++ week3/BusinessCard/app/src/main/java/com/manuflowers/businesscard/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.manuflowers.businesscard
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(mainBottomAppBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
