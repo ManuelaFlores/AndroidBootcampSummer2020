16a9e9e12a11b141ba753b1a2f20bc2f9dfab603
Manuela Flores <fmanuela499@gmail.com>
2020-07-26 03:31:57 -0500

    delete commented code

--- week9/PhotoInspiration/app/src/main/java/com/manuflowers/photoinspiration/ui/home/HomeViewModel.kt
+++ week9/PhotoInspiration/app/src/main/java/com/manuflowers/photoinspiration/ui/home/HomeViewModel.kt
@@ -4,38 +4,14 @@ import androidx.lifecycle.*
 import com.manuflowers.photoinspiration.R
 import com.manuflowers.photoinspiration.application.PhotoInspirationApplication
 import com.manuflowers.photoinspiration.data.PhotoInspirationRepository
-import com.manuflowers.photoinspiration.data.PhotosInspirationRepositoryImpl
-import com.manuflowers.photoinspiration.data.local.database.PhotoInspirationDatabase
-import com.manuflowers.photoinspiration.data.local.database.PhotosDao
-import com.manuflowers.photoinspiration.data.local.preferences.PhotoInspirationPreferences
 import com.manuflowers.photoinspiration.data.models.ErrorNetworkMessage
 import com.manuflowers.photoinspiration.data.models.PhotoEntity
 import com.manuflowers.photoinspiration.data.remote.networking.NetworkStatusChecker
-import com.manuflowers.photoinspiration.data.remote.networking.RemoteApiManager
-import com.manuflowers.photoinspiration.data.remote.networking.buildApiService
 import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val repository: PhotoInspirationRepository
 ) : ViewModel() {
-
-    /* private val apiService by lazy { buildApiService() }
-
-     private val remoteApi by lazy { RemoteApiManager(apiService) }
-
-     private val repositoryImpl: PhotosInspirationRepositoryImpl
-
-     init {
-         val photosDao: PhotosDao =
-             PhotoInspirationDatabase.getDataBase(PhotoInspirationApplication.getAppContext())
-                 .photosDao()
-
-         repositoryImpl = PhotosInspirationRepositoryImpl(
-             remoteApi,
-             photosDao,
-             PhotoInspirationPreferences()
-         )
-     }*/
 
     private val errorNetworkMessageMutableLiveData = MutableLiveData<ErrorNetworkMessage>()
     val errorNetworkMessageLiveData: LiveData<ErrorNetworkMessage>
