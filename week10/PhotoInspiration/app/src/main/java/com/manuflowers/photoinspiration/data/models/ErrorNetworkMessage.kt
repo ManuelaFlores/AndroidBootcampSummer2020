5a8ac7daa919a61ddc6ff19bdb413adbea34d22b
Manuela Flores <fmanuela499@gmail.com>
2020-08-02 18:56:55 -0500

    delete unnecessary code

--- week10/PhotoInspiration/app/src/main/java/com/manuflowers/photoinspiration/data/models/ErrorNetworkMessage.kt
+++
@@ -1,3 +0,0 @@
-package com.manuflowers.photoinspiration.data.models
-
-data class ErrorNetworkMessage(val message: Int)
--- week10/PhotoInspiration/app/src/main/java/com/manuflowers/photoinspiration/data/remote/networking/RemoteApiManager.kt
+++ week10/PhotoInspiration/app/src/main/java/com/manuflowers/photoinspiration/data/remote/networking/RemoteApiManager.kt
@@ -3,7 +3,6 @@ package com.manuflowers.photoinspiration.data.remote.networking
 import com.manuflowers.photoinspiration.data.models.Failure
 import com.manuflowers.photoinspiration.data.models.Success
 
-const val BASE_URL = "https://api.unsplash.com"
 const val CLIENT_ID = "xMgrwRJDt_GLYKsvxJM2b1TUAYenCzlLt0nErWCnc24"
 
 class RemoteApiManager(private val remoteApiService: RemoteApiService) {
